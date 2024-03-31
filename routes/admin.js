dae59dd9a5ebcae193e4261da57ce17bb22d15cc
nscspc <saininaveen933@gmail.com>
2024-03-31 21:12:08 +0530

    addproduct updated

--- routes/admin.js
+++ routes/admin.js
@@ -6,12 +6,12 @@ const Product = require('../models/product');
 // Add Product
 adminRouter.post('/admin/add-product' , adminMiddleware , async (req , res) => {
     try {
-        const { name , description , quantity , price , category } = req.body;
-        // const { name , description , images , quantity , price , category } = req.body; // these names of the field should match with the name of the fields present in product.dart file.
+        // const { name , description , quantity , price , category } = req.body;
+        const { name , description , images , quantity , price , category } = req.body; // these names of the field should match with the name of the fields present in product.dart file.
         let product = new Product({
             name,
             description,
-            // images,
+            images,
             quantity,
             price,
             category,
