9bf345ce8885ecd58639a915a7301afe5cd751e8
nscspc <saininaveen933@gmail.com>
2024-04-04 01:32:49 +0530

    address api added

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const authMiddleware = require('../middleware/authMiddleware');
 const { Product } = require('../models/product');
 const userRouter = express.Router();
 const User = require("../models/user");
+const Order = require("../models/order");
 
 // Add Product
 userRouter.post('/api/add-to-cart' , authMiddleware , async (req , res) => {
@@ -71,5 +72,64 @@ userRouter.delete('/api/remove-from-cart/:id' , authMiddleware , async (req , re
         res.status(500).json({error:e.message});
     }
 });
+
+// save user address
+userRouter.post("/api/save-user-address", auth, async (req, res) => {
+    try {
+      const { address } = req.body;
+      let user = await User.findById(req.user);
+      user.address = address;
+      user = await user.save();
+      res.json(user);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  });
+  
+  // order product
+  userRouter.post("/api/order", auth, async (req, res) => {
+    try {
+      const { cart, totalPrice, address } = req.body;
+      let products = [];
+  
+      for (let i = 0; i < cart.length; i++) {
+        let product = await Product.findById(cart[i].product._id);
+        if (product.quantity >= cart[i].quantity) { // checking stock availability.
+          product.quantity -= cart[i].quantity;
+          products.push({ product, quantity: cart[i].quantity });
+          await product.save();
+        } else {
+          return res
+            .status(400) // bad request.
+            .json({ msg: `${product.name} is out of stock!` });
+        }
+      }
+  
+      let user = await User.findById(req.user);
+      user.cart = [];
+      user = await user.save();
+  
+      let order = new Order({
+        products,
+        totalPrice,
+        address,
+        userId: req.user,
+        orderedAt: new Date().getTime(),
+      });
+      order = await order.save();
+      res.json(order);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  });
+  
+  userRouter.get("/api/orders/me", auth, async (req, res) => {
+    try {
+      const orders = await Order.find({ userId: req.user });
+      res.json(orders);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  });
 
 module.exports = userRouter;
